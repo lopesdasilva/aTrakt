5b4190add6191b715dcffef5ed64cc2fa9f505c3
lopesdasilva <lopesdasilva.rui@gmail.com>
2014-04-04 09:16:51 +0100

    widget working v1

--- Trakt-fragments/src/main/java/com/lopesdasilva/trakt/widgets/MyFirstWidget.java
+++ Trakt-fragments/src/main/java/com/lopesdasilva/trakt/widgets/MyFirstWidget.java
@@ -1,15 +1,16 @@
 package com.lopesdasilva.trakt.widgets;
 
+import android.app.LauncherActivity;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
 import android.widget.Toast;
 
 import com.androidquery.AQuery;
@@ -20,99 +21,136 @@ import com.lopesdasilva.trakt.Tasks.DownloadDayCalendar;
 import com.lopesdasilva.trakt.extras.UserChecker;
 
 import java.util.Date;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
  * Created by NB20308 on 03-04-2014.
  */
-public class MyFirstWidget extends AppWidgetProvider implements DownloadDayCalendar.OnDayCalendarTaskCompleted {
+public class MyFirstWidget extends AppWidgetProvider {
 
     private ServiceManager manager;
-    private AppWidgetManager appWidgetManager;
+    public AppWidgetManager appWidgetManager;
     private int[] appWidgetIds;
     private Context context;
 
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-       this.appWidgetManager=appWidgetManager;
-        this.appWidgetIds=appWidgetIds;
-        this.context=context;
-        Toast.makeText(context,"updating widget trakt",Toast.LENGTH_SHORT).show();
-        Log.d("Trakt it", "TODO CODE TO UDPATE THE LIST");
-        manager = UserChecker.checkUserLogin(context);
+        this.appWidgetManager = appWidgetManager;
+        this.appWidgetIds = appWidgetIds;
+        this.context = context;
+        Toast.makeText(context, "updating widget trakt", Toast.LENGTH_SHORT).show();
+        Log.d("trakt it", "onupdate widget");
+
+
+
+        for (int i = 0; i < appWidgetIds.length; ++i) {
+
+            Intent intent = new Intent(context, WidgetService.class);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+
+            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
+            Log.d("trakt it", "setting remote adapter "+appWidgetIds[i]);
+            rv.setRemoteAdapter(R.id.listViewWidgetTonight, intent);
+
+//            rv.setEmptyView(R.id.list, R.id.empty_view);
+            appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
+        }
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+
+    }
+
+
+}
+
+
+
+
+
+/**
+ * If you are familiar with Adapter of ListView,this is the same as adapter
+ * with few changes
+ */
+class ListProvider implements RemoteViewsService.RemoteViewsFactory, DownloadDayCalendar.OnDayCalendarTaskCompleted {
+
+    private final Context mContext;
+    private final int mAppWidgetId;
+    private ServiceManager manager;
+    private List<CalendarDate> mTonightShows = new LinkedList<CalendarDate>();
+
+    public ListProvider(Context context, Intent intent) {
+        mContext = context;
+        mAppWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d("Trakt it", "ListProvider onCreate");
+
+        manager = UserChecker.checkUserLogin(mContext);
         new DownloadDayCalendar(this, manager, new Date()).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
-
-
+        Log.d("Trakt it", "Download started");
 
     }
 
     @Override
+    public void onDataSetChanged() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        mTonightShows.clear();
+    }
+
+    @Override
+    public int getCount() {
+        return mTonightShows.size();
+    }
+
+    @Override
+    public RemoteViews getViewAt(int position) {
+        RemoteViews rv = new RemoteViews(mContext.getPackageName(), R.layout.widget_tonight_item);
+rv.setTextViewText(R.id.textViewWidgetShowTitle,mTonightShows.get(position).episodes.get(0).show.title);
+        rv.setTextViewText(R.id.textViewWidgetEpisodeInfo,"S"+mTonightShows.get(position).episodes.get(0).episode.season+"E"+mTonightShows.get(position).episodes.get(0).episode.number+" "+mTonightShows.get(position).episodes.get(0).episode.title);
+        AQuery aq= new AQuery(mContext);
+        Bitmap screen = aq.getCachedImage(mTonightShows.get(position).episodes.get(0).episode.images.screen);
+        rv.setImageViewBitmap(R.id.imageViewWidgetScreen,screen);
+
+        return rv;
+    }
+
+    @Override
+    public RemoteViews getLoadingView() {
+        return null;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return 1;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+    @Override
     public void OnDayCalendarTaskCompleted(List<CalendarDate> response) {
-        Log.d("Trakt it", "WIDGET: Download complete START TO UPDATE");
-        final int N = appWidgetIds.length;
-        // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int i = 0; i < N; i++) {
-            int appWidgetId = appWidgetIds[i];
-            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.example_appwidget);
-            AQuery aq = new AQuery(context);
+        mTonightShows = response;
+        Log.d("trakt it", "download complete");
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(mContext);
 
-views.setRemoteAdapter();
-
-
-
-//           views.setImageViewBitmap(R.id.imageViewWidgetScreen, aq.getCachedImage("http://slurm.trakt.us/images/episodes/15859-3-3-218.jpg"));
-            // Get the layout for the App Widget and attach an on-click listener
-            // to the button
-
-//            views.setOnClickPendingIntent(R.id.button, pendingIntent);
-//
-            // Tell the AppWidgetManager to perform an update on the current app widget
-            appWidgetManager.updateAppWidget(appWidgetId, views);
-        }
+        appWidgetManager.notifyAppWidgetViewDataChanged(mAppWidgetId,R.id.listViewWidgetTonight);
     }
-
-    public class EpisodeTonightAdapter extends BaseAdapter {
-
-        private final List<CalendarDate.CalendarTvShowEpisode> mListEpisodes;
-        private final LayoutInflater inflater;
-
-        public EpisodeTonightAdapter(Context context, List<CalendarDate.CalendarTvShowEpisode> mListepisodes) {
-            this.mListEpisodes = mListepisodes;
-            inflater = LayoutInflater.from(context);
-        }
-
-        @Override
-        public int getCount() {
-            return mListEpisodes.size();
-        }
-
-        @Override
-        public CalendarDate.CalendarTvShowEpisode getItem(int i) {
-            return mListEpisodes.get(i);
-        }
-
-        @Override
-        public long getItemId(int i) {
-            return i;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            if (convertView == null) {
-                convertView = inflater.inflate(R.layout.widget_tonight_item, parent, false);
-            }
-            AQuery aq = new AQuery(convertView);
-
-            aq.id(R.id.imageViewDrawerEpisodeScreen).image(mListEpisodes.get(position).episode.images.screen, true, true, 0, 0, null, AQuery.FADE_IN);
-            aq.id(R.id.textViewDrawerEpisodeTitle).text(mListEpisodes.get(position).episode.title);
-            aq.id(R.id.textViewDrawerEpisodeNumber).text("S" + mListEpisodes.get(position).episode.season + "E" + mListEpisodes.get(position).episode.number);
-            aq.id(R.id.textViewDrawerEpisodeDate).text(mListEpisodes.get(position).show.title);
-            aq.id(R.id.textViewDrawerEpisodeNetwork).text(mListEpisodes.get(position).show.network);
-            if (mListEpisodes.get(position).episode.watched)
-                aq.id(R.id.imageViewDrawerEpisodeSeenTag).visible();
-            else
-                aq.id(R.id.imageViewDrawerEpisodeSeenTag).gone();
-            return convertView;
-        }
-    }
-
 }
+
+
+
---
+++ Trakt-fragments/src/main/java/com/lopesdasilva/trakt/widgets/WidgetService.java
@@ -0,0 +1,23 @@
+package com.lopesdasilva.trakt.widgets;
+
+import android.appwidget.AppWidgetManager;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.RemoteViewsService;
+
+public class WidgetService extends RemoteViewsService {
+/*
+* So pretty simple just defining the Adapter of the listview
+* here Adapter is ListProvider
+* */
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+
+        int appWidgetId = intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,
+                AppWidgetManager.INVALID_APPWIDGET_ID);
+        Log.d("trakt it", "onGetViewFactory widgetID:" + appWidgetId);
+        return (new ListProvider(this.getApplicationContext(), intent));
+    }
+
+}
